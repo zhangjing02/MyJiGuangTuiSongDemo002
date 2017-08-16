7ebf540af1881bdcf0240e4c97cb4c4309e2a5ba
zhangjing02 <zhangjing574839@163.com>
2017-08-16 19:33:02 +0800

    这是一个极光推送

---
+++ app/src/main/java/com/senssun/www/myjiguangtuisongdemo002/MainActivity.java
@@ -0,0 +1,75 @@
+package com.senssun.www.myjiguangtuisongdemo002;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import cn.jpush.android.api.InstrumentedActivity;
+
+public class MainActivity extends InstrumentedActivity {
+	public static boolean isForeground = false;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		registerMessageReceiver();  // used for receive msg
+	}
+	@Override
+	protected void onResume() {
+		isForeground = true;
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		isForeground = false;
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+		super.onDestroy();
+	}
+	
+
+	//for receive customer msg from jpush server
+	private MessageReceiver mMessageReceiver;
+	public static final String MESSAGE_RECEIVED_ACTION = "com.example.jpushdemo.MESSAGE_RECEIVED_ACTION";
+	public static final String KEY_TITLE = "title";
+	public static final String KEY_MESSAGE = "message";
+	public static final String KEY_EXTRAS = "extras";
+	
+	public void registerMessageReceiver() {
+		mMessageReceiver = new MessageReceiver();
+		IntentFilter filter = new IntentFilter();
+		filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+		filter.addAction(MESSAGE_RECEIVED_ACTION);
+		LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, filter);
+	}
+
+	public class MessageReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			try {
+				if (MESSAGE_RECEIVED_ACTION.equals(intent.getAction())) {
+					String messge = intent.getStringExtra(KEY_MESSAGE);
+					String extras = intent.getStringExtra(KEY_EXTRAS);
+					StringBuilder showMsg = new StringBuilder();
+					showMsg.append(KEY_MESSAGE + " : " + messge + "\n");
+					if (!ExampleUtil.isEmpty(extras)) {
+						showMsg.append(KEY_EXTRAS + " : " + extras + "\n");
+					}
+					Log.i("hhhhhhhhhhhhh", "onReceive: 这里获取的是啥？"+showMsg.toString());
+				}
+			} catch (Exception e){
+			}
+		}
+	}
+
+}
